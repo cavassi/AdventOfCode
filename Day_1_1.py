0dd37cb72e7ffc9afc6cf1d8cd8c24e9a1018a55
cavassi <noa2009@live.se>
2022-12-02 11:14:08 +0100

    Day 1 Complete

---
+++ Day_1_1.py
@@ -0,0 +1,23 @@
+with open('data.txt', 'r') as file:
+    line = file.read()
+
+line = line.rstrip().split("\n\n")
+
+current_value = 0
+highest_value = 0
+
+for elf in range(len(line)):
+    line[elf] = line[elf].split("\n")
+
+for i in range(len(line)):
+    for j in range(len(line[i])):
+        line[i][j] = int(line[i][j])
+
+for elf in line:
+    current_value = 0
+    for food in elf:
+        current_value += food
+    if current_value > highest_value:
+        highest_value = current_value
+
+print(highest_value)
---
+++ Day_1_2.py
@@ -0,0 +1,25 @@
+with open('data.txt', 'r') as file:
+    line = file.read()
+
+line = line.rstrip().split("\n\n")
+
+current_value = 0
+highest_value = 0
+list_of_elfs = []
+
+for elf in range(len(line)):
+    line[elf] = line[elf].split("\n")
+
+for i in range(len(line)):
+    for j in range(len(line[i])):
+        line[i][j] = int(line[i][j])
+
+for elf in line:
+    current_value = 0
+    for food in elf:
+        current_value += food
+    list_of_elfs.append(current_value)
+
+list_of_elfs.sort()
+
+print(list_of_elfs[-3] + list_of_elfs[-2] + list_of_elfs[-1])
