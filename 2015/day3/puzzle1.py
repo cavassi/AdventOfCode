a526537c98bb80ad452d3d5f6b28a0819af975e5
cavassi <noa2009@live.se>
2023-02-28 00:59:16 +0100

    day3

--- 2015/day3/puzzle1.py
+++ 2015/day3/puzzle1.py
@@ -2,9 +2,17 @@ with open("day3/data.txt", "r") as data:
     data = data.read()
 
 location = [0,0]
-list_of_houses = [0,0]
+list_of_houses = [(0,0)]
+a = False
+
+def check_if_in_list(location):
+    if location in list_of_houses:
+        return True
+    else:
+        return location
 
 for i in range(len(data)):
+    a = False
     if data[i] == "^":
         location[1] += 1
     if data[i] == ">":
@@ -13,9 +21,15 @@ for i in range(len(data)):
         location[1] += -1
     if data[i] == "<":
         location[0] += -1
-    
-    for j in range(len(list_of_houses)):
-        if location not in list_of_houses:
-            list_of_houses.append(location)
 
-print(list_of_houses)
+    for j in range(len(list_of_houses)):
+        if location[0] == list_of_houses[j][0] and \
+            location[1] == list_of_houses[j][1]:
+            a = True
+            break
+    if a == True:
+        continue
+    else:
+        list_of_houses.append(tuple(location))
+
+print(len(list_of_houses))
--- 2015/day3/puzzle2.py
+++ 2015/day3/puzzle2.py
@@ -1,25 +1,48 @@
-with open("day2/data.txt", "r") as data:
+with open("day3/data.txt", "r") as data:
     data = data.read()
 
-data = data.split("\n")
-presents = []
-total_ribbon = 0
-
-smallest = 0
-second_smallest = 0
-biggest = 0
+santa_location = [0,0]
+robot_location = [0,0]
+list_of_houses = [(0,0)]
+a = False
 
 for i in range(len(data)):
-    presents.append(data[i].split("x"))
+    a = False
+    if i % 2 == 0:
+        if data[i] == "^":
+            santa_location[1] += 1
+        if data[i] == ">":
+            santa_location[0] += 1
+        if data[i] == "v":
+            santa_location[1] += -1
+        if data[i] == "<":
+            santa_location[0] += -1
+        for k in range(len(list_of_houses)):
+            if (santa_location[0] == list_of_houses[k][0] and \
+                santa_location[1] == list_of_houses[k][1]):
+                a = True
+                break
+    elif i % 2 != 0:
+        if data[i] == "^":
+            robot_location[1] += 1
+        if data[i] == ">":
+            robot_location[0] += 1
+        if data[i] == "v":
+            robot_location[1] += -1
+        if data[i] == "<":
+            robot_location[0] += -1
+        for l in range(len(list_of_houses)):
+            if (robot_location[0] == list_of_houses[l][0] and \
+                robot_location[1] == list_of_houses[l][1]):
+                a = True
+                break
 
-for i in range(len(presents)):
-    smallest = min(int(presents[i][0]),int(presents[i][1]),int(presents[i][2]))
+    if a == True:
+        continue
+    else:
+        if i % 2 == 0:
+            list_of_houses.append(tuple(santa_location))
+        else:
+            list_of_houses.append(tuple(robot_location))
 
-    second_smallest = (int(presents[i][0]) + int(presents[i][1]) + int(presents[i][2])) -\
-                        (smallest + max(int(presents[i][0]),int(presents[i][1]),int(presents[i][2])))
-
-    biggest = max(int(presents[i][0]),int(presents[i][1]),int(presents[i][2]))
-
-    total_ribbon += (2*(smallest + second_smallest) + (smallest*second_smallest*biggest))
-
-print(total_ribbon)
+print(len(list_of_houses))
